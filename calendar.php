ad82e70c2f90878e9a96b6cde4e30f8257f16ef4
rybkinevg <boomuk-mc@yandex.ru>
2020-11-11 18:57:22 +0300

    2

---
+++ calendar.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Calendar for posts
+ * Plugin URI: https://github.com/rybkinevg/calendar
+ * Description: Плагин для добавления календаря для разных типов записей
+ * Version: 1
+ * Author: Евгений Рыбкин
+ * Author URI: https://github.com/rybkinevg
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
