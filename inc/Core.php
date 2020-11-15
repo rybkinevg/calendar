383bda17d6b80918dc96b898bd94552cb2355381
rybkinevg <boomuk-mc@yandex.ru>
2020-11-15 23:19:36 +0300

    update

--- inc/Core.php
+++ inc/Core.php
@@ -26,15 +26,13 @@ class WPPC_Core
         new WPPC_Admin();
         require_once(WPPC_DIR . 'inc/Ajax.php');
         new WPPC_Ajax();
+        require_once(WPPC_DIR . 'inc/Views.php');
+        new WPPC_Views();
     }
 
     public function hooks()
     {
-        // Подключение скриптов и стилей
         add_action('wp_enqueue_scripts', [$this, 'enqueue']);
-
-        // Фильтрация вывода шаблона страницы архива записей и отдельной страницы записи
-        add_filter('template_include', [$this, 'get_templates']);
     }
 
     public function enqueue()
@@ -84,19 +82,5 @@ class WPPC_Core
             wp_enqueue_script('wppc-script');
             wp_enqueue_script('wppc-jquery');
         }
-    }
-
-    public function get_templates($template)
-    {
-        if (is_archive(self::$post_type)) {
-            if ($new_template = WPPC_DIR . 'templates/template_events.php')
-                $template = $new_template;
-        }
-        if (is_singular(self::$post_type)) {
-            if ($new_template = WPPC_DIR . 'templates/template_single_event.php')
-                $template = $new_template;
-        }
-
-        return $template;
     }
 }
---
+++ inc/Views.php
@@ -0,0 +1,28 @@
+<?php
+
+class WPPC_Views
+{
+    public function __construct()
+    {
+        $this->hooks();
+    }
+
+    public function hooks()
+    {
+        add_filter('template_include', [$this, 'get_templates']);
+    }
+
+    public function get_templates($template)
+    {
+        if (is_archive(WPPC_Core::$post_type)) {
+            if ($new_template = WPPC_DIR . 'templates/template_events.php')
+                $template = $new_template;
+        }
+        if (is_singular(WPPC_Core::$post_type)) {
+            if ($new_template = WPPC_DIR . 'templates/template_single_event.php')
+                $template = $new_template;
+        }
+
+        return $template;
+    }
+}
